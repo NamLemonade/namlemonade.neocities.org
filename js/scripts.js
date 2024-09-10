69e22120d1e29c8edba5c5242c647eced4854e5a
Nam Lemonade <larouchejade11@gmail.com>
2024-09-09 20:45:54 -0400

    I did some edits here idc i'm talkin to friends

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', (event) => {
-	new cursoreffects.rainbowCursor();
-});
+// window.addEventListener('load', (event) => {
+// 	new cursoreffects.rainbowCursor();
+// });
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
