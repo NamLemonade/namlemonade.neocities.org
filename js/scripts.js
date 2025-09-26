cf03b8fdb80b694b74e1250764e4fb389ff9c3c7
Nam Lemonade <larouchejade11@gmail.com>
2025-09-26 19:48:57 -0400

    Slight edits. Not online yet

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,18 @@ $('html').removeClass();
 
 if (localStorage.getItem('pageTheme') == null) {
 	let randomTheme = Math.floor(Math.random() * themesArray.length);
-	localStorage.setItem('pageTheme', themesArray[randomTheme]);
+	let randomNumber = Math.floor(Math.random() * 50);
+
+	// RARE AND SPECIAL THEME
+	if (randomNumber === 1) {
+		console.log(
+			"%cCONGRATS! You're lucky enough to be GREEN TODAY!",
+			'background-color: green; color: white; font-style: bold; border: 1px solid limegreen; font-size: 14px; padding: 5px 10px; border-radius:8px;'
+		);
+		localStorage.setItem('pageTheme', 'theme-green');
+	} else {
+		localStorage.setItem('pageTheme', themesArray[randomTheme]);
+	}
 }
 
 $('html').addClass(localStorage.getItem('pageTheme'));
@@ -22,10 +33,26 @@ $('html').addClass(localStorage.getItem('pageTheme'));
 $('.button-switch-theme').on('click', function () {
 	$('html').removeClass();
 	cursorEffect.destroy();
-	if (localStorage.getItem('pageTheme') == 'theme-land') {
-		localStorage.setItem('pageTheme', 'theme-sea');
-		cursorEffect = new cursoreffects.bubbleCursor();
-	} else if (localStorage.getItem('pageTheme') == 'theme-sea') {
+	let randomNumber = Math.floor(Math.random() * 50);
+
+	if (localStorage.getItem('pageTheme') != 'theme-green') {
+		if (randomNumber === 1) {
+			console.log(
+				"%cCONGRATS! You're lucky enough to be GREEN TODAY!",
+				'background-color: green; color: white; font-style: bold; border: 1px solid limegreen; font-size: 14px; padding: 5px 10px; border-radius:8px;'
+			);
+			localStorage.setItem('pageTheme', 'theme-green');
+			cursorEffect = new cursoreffects.bubbleCursor();
+		} else {
+			if (localStorage.getItem('pageTheme') == 'theme-land') {
+				localStorage.setItem('pageTheme', 'theme-sea');
+				cursorEffect = new cursoreffects.bubbleCursor();
+			} else if (localStorage.getItem('pageTheme') == 'theme-sea') {
+				localStorage.setItem('pageTheme', 'theme-land');
+				cursorEffect = new cursoreffects.rainbowCursor();
+			}
+		}
+	} else {
 		localStorage.setItem('pageTheme', 'theme-land');
 		cursorEffect = new cursoreffects.rainbowCursor();
 	}
@@ -35,10 +62,27 @@ $('.button-switch-theme').on('click', function () {
 	window.scrollTo(0, 0);
 });
 
+$('.secret-green-backdoor-shhhh').on('click', function () {
+	$('html').removeClass();
+	cursorEffect.destroy();
+
+	console.log(
+		"%cYou found the secret green button. Shhh don't tell people about it.",
+		'background-color: green; color: white; font-style: bold; border: 1px solid limegreen; font-size: 14px; padding: 5px 10px; border-radius:8px;'
+	);
+	localStorage.setItem('pageTheme', 'theme-green');
+	cursorEffect = new cursoreffects.bubbleCursor();
+	$('html').addClass(localStorage.getItem('pageTheme'));
+
+	window.scrollTo(0, 0);
+});
+
 window.addEventListener('load', (event) => {
 	if (localStorage.getItem('pageTheme') == 'theme-land') {
 		cursorEffect = new cursoreffects.rainbowCursor();
 	} else if (localStorage.getItem('pageTheme') == 'theme-sea') {
+		cursorEffect = new cursoreffects.bubbleCursor();
+	} else if (localStorage.getItem('pageTheme') == 'theme-green') {
 		cursorEffect = new cursoreffects.bubbleCursor();
 	}
 });
